59cb1c58e1c1193744a951a233f38e3e3b740ff3
raman <ramanisstudent@gmail.com>
2021-01-04 19:35:38 +0545

    changed skill specification and color of R

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,2 @@
 gsap.from("#greet", { duration: 1, y: "-1000%", ease: "bounce" });
-gsap.from("#intro", { delay: 1, duration: 1, y: "-1000%", ease: "bounce" });
 gsap.from("#roletag", { delay: 2, duration: 1, y: "-1000%", ease: "bounce" });
-
-gsap.from(".buttons", { duration: 3, ease: "slow", opacity: 0 });
-gsap.from("#thingsiknow", { delay: 3, duration: 1, x: "-1000%" });
